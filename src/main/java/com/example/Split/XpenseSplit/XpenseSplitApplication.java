51b522d67a261adc3cae4240d9f936f6c61c0bb8
pranav-nani <pranavreddy1412@gmail.com>
2025-08-17 19:42:27 +0530

    first commit

---
+++ src/main/java/com/example/Split/XpenseSplit/XpenseSplitApplication.java
@@ -0,0 +1,13 @@
+package com.example.Split.XpenseSplit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpenseSplitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpenseSplitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Split/XpenseSplit/XpenseSplitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Split.XpenseSplit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpenseSplitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
