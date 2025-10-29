c91f5b3abc36e1a1746fcb5c5251528d42a7264a
pranav-nani <pranavreddy1412@gmail.com>
2025-10-29 12:54:45 +0530

    waking up backend using ping endpoint

---
+++ src/main/java/com/example/Split/XpenseSplit/controller/LoadingController.java
@@ -0,0 +1,15 @@
+package com.example.Split.XpenseSplit.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class LoadingController {
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("pong");
+    }
+}
